421186adf5c3f3f322d2453d2cf5d1c3b1be2d36
Roman Ivanov <romani@epayservices.com>
2022-10-26 21:42:25 +0500

    remove: extra code

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,21 +2,6 @@ class TestsController < Simpler::Controller
 
   def index
     render 'tests/show'
-
-    # render plain: "Plain text"
-
-    # @time = Time.now
-    # render inline: "<p>Time: <%= @time %></p>"
-
-    # render html: "<p>It's <strong>STRONG</strong></p>"
-
-    # @test = Test.first
-    # status 201
-    # headers['X-Content'] = "XXXXX"
-    # render json: @test
-
-    # @test = Test.first
-    # render xml: @test
   end
 
   def create
@@ -24,5 +9,4 @@ class TestsController < Simpler::Controller
 
   def show
   end
-
 end
